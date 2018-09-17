338d3c29d9ef9d0909c364e55070f0b92c67684e
Jonathan Ferguson <jono@underscore.io>
2018-09-17 14:38:28 +1000

    elm-format

--- src/Csv.elm
+++ src/Csv.elm
@@ -18,6 +18,7 @@ The results are provided as lists.
 
 -}
 
+
 {-| The `Csv` type structure.
 -}
 type alias Csv =
@@ -27,154 +28,214 @@ type alias Csv =
 
 
 parse : String -> Csv
-parse = parseWith ','
+parse =
+    parseWith ','
 
 
 parseWith : Char -> String -> Csv
 parseWith separator raw =
     let
-        values = List.reverse (splitWith separator raw)
-    in    
-      case values of
-          []    -> { headers = [], records = []}
-          x::xs -> { headers = x,  records = xs}  
+        values =
+            List.reverse (splitWith separator raw)
+    in
+    case values of
+        [] ->
+            { headers = [], records = [] }
 
+        x :: xs ->
+            { headers = x, records = xs }
 
 
 splitWith : Char -> String -> List (List String)
 splitWith separator raw =
-  let
-    state    = String.foldl accumulator (start separator) raw
-    ss       = handleSeparator True state 
-    sRow     = Maybe.map (\a -> (::) (List.reverse a) ss.content) ss.cRow
-    nContent = Maybe.withDefault ss.content sRow    
-  in
-    nContent
-        
+    let
+        state =
+            String.foldl accumulator (start separator) raw
 
- 
+        ss =
+            handleSeparator True state
+
+        sRow =
+            Maybe.map (\a -> (::) (List.reverse a) ss.content) ss.cRow
+
+        nContent =
+            Maybe.withDefault ss.content sRow
+    in
+    nContent
+
 
 type alias State =
-  { separator          : Char 
-  , cValue             : Maybe String
-  , cRow               : Maybe (List String)
-  , content            : List  (List String)
-  , inQuote            : Bool  
-  , previousChar       : Maybe Char   
-  }
+    { separator : Char
+    , cValue : Maybe String
+    , cRow : Maybe (List String)
+    , content : List (List String)
+    , inQuote : Bool
+    , previousChar : Maybe Char
+    }
 
-start s = { separator = s
-        , cValue = Nothing
-        , cRow   = Nothing
-        , content = []
-        , inQuote = False
-        , previousChar = Nothing}
- 
 
-accumulator :  Char -> State -> State
+start s =
+    { separator = s
+    , cValue = Nothing
+    , cRow = Nothing
+    , content = []
+    , inQuote = False
+    , previousChar = Nothing
+    }
+
+
+accumulator : Char -> State -> State
 accumulator current state =
-  let 
-    newState = case current  of  
-      '"'  -> handleQuote state        
-      '\r' -> handleNewLine current state
-      '\n' -> handleNewLine current state 
-      _    -> handleCharacter current state 
-  in 
-    {newState | previousChar = Just current}
+    let
+        newState =
+            case current of
+                '"' ->
+                    handleQuote state
 
+                -- \r
+                '\u{000D}' ->
+                    handleNewLine current state
 
-handleQuote : State -> State 
-handleQuote state = 
-  case state.inQuote of 
-    True  -> inQuote  state 
-    False -> outQuote state
-   
+                '\n' ->
+                    handleNewLine current state
 
-inQuote : State -> State 
-inQuote state = 
-  case state.previousChar of 
-    Nothing -> {state | inQuote = False} 
-    Just ch ->
-      if ch == '"' || ch == '\\' then
-        let 
-          nV =  Maybe.map  (\ v -> v ++ "\"")  state.cValue
-        in   
-          {state | cValue = nV,  inQuote = False}
-      else   
-        {state | inQuote = False}
-
-
-outQuote :State -> State 
-outQuote state = 
-  case state.previousChar of 
-    Nothing -> {state | inQuote = True}  
-    Just prev -> 
-      if prev == '"' || prev == '\\'  then 
-        case state.cValue of 
-          Nothing -> state  
-          Just cv ->
-            let
-              trimmed =  String.slice 0 -1 cv
-              ncVale  = trimmed ++ "\""
-            in 
-              {state | cValue = (Just ncVale)}
-      else      
-        {state | inQuote = True}
-
-
-
-handleNewLine : Char -> State -> State 
-handleNewLine char s =  
-  if s.inQuote then 
-    appendCharacter char s
-  else
-    let       
-      nContent = case (s.cValue,s.cRow) of 
-        (Nothing, Nothing ) -> s.content
-        (Just c , Nothing ) -> [c]  :: s.content
-        (Nothing, Just r  ) -> List.reverse("" :: r) :: s.content
-        (Just c,  Just r  ) ->  
-          let 
-            nRow = List.reverse(c :: r ) 
-          in 
-            nRow :: s.content
+                _ ->
+                    handleCharacter current state
     in
-    {s | content = nContent, cValue = Nothing, cRow = Nothing}  
+    { newState | previousChar = Just current }
 
 
-handleSeparator : Bool -> State -> State 
-handleSeparator end s = 
-  let 
-    fRow mv mr =
-      case (mv,mr) of 
-        (Nothing, Nothing ) -> 
-          case end of 
-            True  ->  Nothing 
-            False ->  Just [""] 
-        (Just v , Nothing ) -> Just [v] 
-        (Nothing, Just r  ) -> Just ("" :: r)
-        (Just v,  Just r  ) -> Just (v :: r)
-    nRow = fRow s.cValue s.cRow  
-  in
-    {s | cValue = Nothing, cRow = nRow}     
+handleQuote : State -> State
+handleQuote state =
+    case state.inQuote of
+        True ->
+            inQuote state
+
+        False ->
+            outQuote state
 
 
-handleCharacter : Char -> State -> State 
-handleCharacter c s = 
-  if c == s.separator && not s.inQuote then 
-    handleSeparator False s
-  else      
-    appendCharacter c s
+inQuote : State -> State
+inQuote state =
+    case state.previousChar of
+        Nothing ->
+            { state | inQuote = False }
+
+        Just ch ->
+            if ch == '"' || ch == '\\' then
+                let
+                    nV =
+                        Maybe.map (\v -> v ++ "\"") state.cValue
+                in
+                { state | cValue = nV, inQuote = False }
+
+            else
+                { state | inQuote = False }
 
 
-appendCharacter : Char -> State -> State 
-appendCharacter c s = 
-  let 
-    char   = String.fromChar c
-    update = case s.cValue of 
-      Nothing  -> Just char
-      Just cV  -> Just (cV ++ char)
-  in    
-    {s | cValue = update}      
+outQuote : State -> State
+outQuote state =
+    case state.previousChar of
+        Nothing ->
+            { state | inQuote = True }
+
+        Just prev ->
+            if prev == '"' || prev == '\\' then
+                case state.cValue of
+                    Nothing ->
+                        state
+
+                    Just cv ->
+                        let
+                            trimmed =
+                                String.slice 0 -1 cv
+
+                            ncVale =
+                                trimmed ++ "\""
+                        in
+                        { state | cValue = Just ncVale }
+
+            else
+                { state | inQuote = True }
 
 
+handleNewLine : Char -> State -> State
+handleNewLine char s =
+    if s.inQuote then
+        appendCharacter char s
+
+    else
+        let
+            nContent =
+                case ( s.cValue, s.cRow ) of
+                    ( Nothing, Nothing ) ->
+                        s.content
+
+                    ( Just c, Nothing ) ->
+                        [ c ] :: s.content
+
+                    ( Nothing, Just r ) ->
+                        List.reverse ("" :: r) :: s.content
+
+                    ( Just c, Just r ) ->
+                        let
+                            nRow =
+                                List.reverse (c :: r)
+                        in
+                        nRow :: s.content
+        in
+        { s | content = nContent, cValue = Nothing, cRow = Nothing }
+
+
+handleSeparator : Bool -> State -> State
+handleSeparator end s =
+    let
+        fRow mv mr =
+            case ( mv, mr ) of
+                ( Nothing, Nothing ) ->
+                    case end of
+                        True ->
+                            Nothing
+
+                        False ->
+                            Just [ "" ]
+
+                ( Just v, Nothing ) ->
+                    Just [ v ]
+
+                ( Nothing, Just r ) ->
+                    Just ("" :: r)
+
+                ( Just v, Just r ) ->
+                    Just (v :: r)
+
+        nRow =
+            fRow s.cValue s.cRow
+    in
+    { s | cValue = Nothing, cRow = nRow }
+
+
+handleCharacter : Char -> State -> State
+handleCharacter c s =
+    if c == s.separator && not s.inQuote then
+        handleSeparator False s
+
+    else
+        appendCharacter c s
+
+
+appendCharacter : Char -> State -> State
+appendCharacter c s =
+    let
+        char =
+            String.fromChar c
+
+        update =
+            case s.cValue of
+                Nothing ->
+                    Just char
+
+                Just cV ->
+                    Just (cV ++ char)
+    in
+    { s | cValue = update }
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,208 +1,229 @@
-module Tests exposing (..)
+module Tests exposing (suite)
 
+import Csv exposing (parse)
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
-import Csv  exposing (parse)
+
 
 suite : Test
 suite =
     describe "The CSV module"
         [ describe "CSV.parse"
-            [ 
-              test "Convert a string of comma-separated values into a `Csv` structure." <|
+            [ test "Convert a string of comma-separated values into a `Csv` structure." <|
                 \_ ->
                     let
-                         output = Csv.parse "id,value\n51,one\n50,two"
-                         expect =      {
-                            headers = ["id", "value"],
-                            records = [
-                                          ["51", "one"],
-                                          ["50", "two"]
-                                      ]
-                           }
+                        output =
+                            Csv.parse "id,value\n51,one\n50,two"
+
+                        expect =
+                            { headers = [ "id", "value" ]
+                            , records =
+                                [ [ "51", "one" ]
+                                , [ "50", "two" ]
+                                ]
+                            }
                     in
-                        Expect.equal output expect            
+                    Expect.equal output expect
             , test "Convert a string of comma-separated values into a `Csv` structure with trailing newline" <|
                 \_ ->
                     let
-                         output = Csv.parse "id,value\n51,one\n50,two\n"
-                         expect =      {
-                            headers = ["id", "value"],
-                            records = [
-                                          ["51", "one"],
-                                          ["50", "two"]
-                                      ]
-                           }
+                        output =
+                            Csv.parse "id,value\n51,one\n50,two\n"
+
+                        expect =
+                            { headers = [ "id", "value" ]
+                            , records =
+                                [ [ "51", "one" ]
+                                , [ "50", "two" ]
+                                ]
+                            }
                     in
-                        Expect.equal output expect
+                    Expect.equal output expect
             , test "Parses quotes in values " <|
                 \_ ->
                     let
-                         output = Csv.parse "value\nHere is a quote:\"\"\nAnother one:\\\"\n"
-                         expect = {
-                            headers = ["value"],
-                            records = [
-                                ["Here is a quote:\""],
-                                ["Another one:\""]
-                              ]
-                          }
+                        output =
+                            Csv.parse "value\nHere is a quote:\"\"\nAnother one:\\\"\n"
+
+                        expect =
+                            { headers = [ "value" ]
+                            , records =
+                                [ [ "Here is a quote:\"" ]
+                                , [ "Another one:\"" ]
+                                ]
+                            }
                     in
-                        Expect.equal output expect                      
+                    Expect.equal output expect
             , test "Double quotes can be escaped with a backslash or a second quote" <|
                 \_ ->
                     let
-                         output = Csv.parse "value\nHere is a quote:\"\"\nAnother one:\\\""
-                         expect = {
-                           headers = ["value"],
-                           records = [
-                                         ["Here is a quote:\""],
-                                         ["Another one:\""]
-                                     ]
-                          }
+                        output =
+                            Csv.parse "value\nHere is a quote:\"\"\nAnother one:\\\""
+
+                        expect =
+                            { headers = [ "value" ]
+                            , records =
+                                [ [ "Here is a quote:\"" ]
+                                , [ "Another one:\"" ]
+                                ]
+                            }
                     in
-                        Expect.equal output expect
+                    Expect.equal output expect
             , test "Values that contain the character ',' can be quoted " <|
                 \_ ->
                     let
-                         output = Csv.parse "id,value\n\"1,2,3\",\"one,two,three\"\n"
-                         expect = {
-                           headers = ["id", "value"],
-                           records = [
-                                         ["1,2,3", "one,two,three"]
-                                     ]
-                          }
+                        output =
+                            Csv.parse "id,value\n\"1,2,3\",\"one,two,three\"\n"
+
+                        expect =
+                            { headers = [ "id", "value" ]
+                            , records =
+                                [ [ "1,2,3", "one,two,three" ]
+                                ]
+                            }
                     in
-                        Expect.equal output expect
- 
+                    Expect.equal output expect
             , test "Values within quotes can contain new lines " <|
                 \_ ->
                     let
-                         output = Csv.parse """value\n"Here is a multiline \nvalue"\nsingle line value"""
-                         expect =  
-                          {
-                            headers = ["value"],
-                            records = [
-                                        ["Here is a multiline \nvalue"],
-                                        ["single line value"]
-                                      ]
-                          }
+                        output =
+                            Csv.parse """value
+"Here is a multiline 
+value"
+single line value"""
+
+                        expect =
+                            { headers = [ "value" ]
+                            , records =
+                                [ [ "Here is a multiline \nvalue" ]
+                                , [ "single line value" ]
+                                ]
+                            }
                     in
-                        Expect.equal output expect
+                    Expect.equal output expect
             , test "Values within quotes can contain new lines, again " <|
                 \_ ->
                     let
-                         output = Csv.parse "value\n\"Here is a multiline \nvalue - 1,2,3\"\nsingle line value"
-                         expect =  
-                          {
-                            headers = ["value"],
-                            records = [
-                                        ["Here is a multiline \nvalue - 1,2,3"],
-                                        ["single line value"]
-                                      ]
-                          }
+                        output =
+                            Csv.parse "value\n\"Here is a multiline \nvalue - 1,2,3\"\nsingle line value"
+
+                        expect =
+                            { headers = [ "value" ]
+                            , records =
+                                [ [ "Here is a multiline \nvalue - 1,2,3" ]
+                                , [ "single line value" ]
+                                ]
+                            }
                     in
-                        Expect.equal output expect
+                    Expect.equal output expect
             , test "Can handle whitespace " <|
                 \_ ->
                     let
-                         output = Csv.parse "h1,h2,h3\nfull, ,\t\n,full,\n,,full\n"
-                         expect =  
-                          {
-                            headers = ["h1","h2","h3"],
-                            records = [
-                                        ["full"," ","\t"],
-                                        ["","full",""],
-                                        ["","","full"]
-                                      ]
-                          }
+                        output =
+                            Csv.parse "h1,h2,h3\nfull, ,\t\n,full,\n,,full\n"
+
+                        expect =
+                            { headers = [ "h1", "h2", "h3" ]
+                            , records =
+                                [ [ "full", " ", "\t" ]
+                                , [ "", "full", "" ]
+                                , [ "", "", "full" ]
+                                ]
+                            }
                     in
-                        Expect.equal output expect
+                    Expect.equal output expect
             , test "Can handle empty values " <|
                 \_ ->
                     let
-                         output = Csv.parse "\n\n\n"
-                         expect =  
-                          {
-                            headers = [],
-                            records = []
-                          }
+                        output =
+                            Csv.parse "\n\n\n"
+
+                        expect =
+                            { headers = []
+                            , records = []
+                            }
                     in
-                        Expect.equal output expect
-            ,  test "minimum viable csv " <|
+                    Expect.equal output expect
+            , test "minimum viable csv " <|
                 \_ ->
                     let
-                         output = Csv.parse ",\n,\n,"
-                         expect =  
-                          {
-                            headers = ["",""],
-                            records = [["",""],["",""]]
-                          }
+                        output =
+                            Csv.parse ",\n,\n,"
+
+                        expect =
+                            { headers = [ "", "" ]
+                            , records = [ [ "", "" ], [ "", "" ] ]
+                            }
                     in
-                        Expect.equal output expect
+                    Expect.equal output expect
             , test "minimum viable csv, trailing \n" <|
                 \_ ->
                     let
-                         output = Csv.parse ",\n,\n,\n"
-                         expect =  
-                          {
-                            headers = ["",""],
-                            records = [["",""],["",""]]
-                          }
+                        output =
+                            Csv.parse ",\n,\n,\n"
+
+                        expect =
+                            { headers = [ "", "" ]
+                            , records = [ [ "", "" ], [ "", "" ] ]
+                            }
                     in
-                        Expect.equal output expect
+                    Expect.equal output expect
             , test "empty string" <|
                 \_ ->
                     let
-                         output = Csv.parse ""
-                         expect =  
-                          {
-                            headers = [],
-                            records = []
-                          }
+                        output =
+                            Csv.parse ""
+
+                        expect =
+                            { headers = []
+                            , records = []
+                            }
                     in
-                        Expect.equal output expect
+                    Expect.equal output expect
             , test "header only" <|
                 \_ ->
                     let
-                         output = Csv.parse "Header"
-                         expect =  
-                          {
-                            headers = ["Header"],
-                            records = []
-                          }
+                        output =
+                            Csv.parse "Header"
+
+                        expect =
+                            { headers = [ "Header" ]
+                            , records = []
+                            }
                     in
-                        Expect.equal output expect
-            ],
-            describe "CSV.parseWith"
-            [ 
-              test "Convert a string of values separated by a _separator_ into a `Csv` structure." <|
+                    Expect.equal output expect
+            ]
+        , describe "CSV.parseWith"
+            [ test "Convert a string of values separated by a _separator_ into a `Csv` structure." <|
                 \_ ->
                     let
-                         output = Csv.parseWith ';' "id;value\n1;one\n2;two\n"
-                         expect =      
-                          {
-                           headers = ["id", "value"],
-                           records = [
-                                         ["1", "one"],
-                                         ["2", "two"]
-                                     ]
-                          } 
-                    in
-                        Expect.equal output expect
+                        output =
+                            Csv.parseWith ';' "id;value\n1;one\n2;two\n"
 
+                        expect =
+                            { headers = [ "id", "value" ]
+                            , records =
+                                [ [ "1", "one" ]
+                                , [ "2", "two" ]
+                                ]
+                            }
+                    in
+                    Expect.equal output expect
             , test "Convert a string of values separated by a non-standard _separator_ into a `Csv` structure." <|
                 \_ ->
                     let
-                         output = Csv.parseWith '☃' "id☃value\n1☃one\n2☃two"
-                         expect =  {
-                           headers = ["id", "value"],
-                           records = [
-                                         ["1", "one"],
-                                         ["2", "two"]
-                                     ]
-                          } 
+                        output =
+                            Csv.parseWith '☃' "id☃value\n1☃one\n2☃two"
+
+                        expect =
+                            { headers = [ "id", "value" ]
+                            , records =
+                                [ [ "1", "one" ]
+                                , [ "2", "two" ]
+                                ]
+                            }
                     in
-                        Expect.equal output expect
+                    Expect.equal output expect
             ]
         ]
